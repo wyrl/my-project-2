211a610e325519a319a3866324a7297f948feb34
Wyr'l Villazorda <wyrlvillazorda@gmail.com>
2024-06-23 01:55:07 +0900

    initial

---
+++ backend.js
@@ -0,0 +1,28 @@
+import express from 'express'
+const app = express()
+const hostname = '0.0.0.0'
+const port = 8080
+import data from './mock_data.json' assert { type: 'json' }
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const fakeAuthorization = 'dGVzdDpzZWNyZXQ='
+
+const CODE_UNAUTHORIZED = 401
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,POST'
+  })
+)
+
+app.get('/persons', (req, res) => {
+  res.json(data)
+})
+
+app.listen(port, hostname, () => {
+  console.log(`listening on port ${hostname}:${port}`)
+})
